353dc6e1076e452a1573105d3c705d9b79b7ee8a
alexandru.codescu <alexandru.codescu@simartis.com>
2023-10-29 09:49:20 +0200

    fix test

--- hello.test.js
+++ hello.test.js
@@ -1,6 +1,8 @@
 const hello = require("./hello");
-describe("My hello", () => {
-  test("works", () => {
-    expect(hello.hello()).toEqual("Hello World from the Office Hours in the terminal!");
+describe("Text format", () => {
+  test("Format like", () => {
+    // Format: 'Hello World executed at DayOfWeek Mon dd YYYY hh24:mm:sec GMT+0000 (Coordinated Universal Time)'
+    var expectedPattern = /Hello World executed at [a-zA-Z]{3} [a-zA-Z]{3} \d{1,2} \d{4} \d{2}\:\d{2}:\d{2} GMT\+\d{4} \([a-zA-Z\s]+\)/;
+    expect(hello.hello()).toMatch(expectedPattern);
   });
 });
